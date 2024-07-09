901d6a4cdcf4734e0e035fdc57159c9884c00846
Lazaro-Costa <lazarocosta.dev@gmail.com>
2024-07-09 11:19:46 -0300

    Config GraphQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ConfigModule } from '@nestjs/config'
-import { DatabaseModule } from './database/database.module';
+import { DatabaseModule } from './database/database.module'
+import { GraphQLModule } from '@nestjs/graphql'
+import path from 'path'
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+      autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
