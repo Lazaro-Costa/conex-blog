2326d769e0f64327dfbbf87155c040636ae17059
Lazaro-Costa <lazarocosta.dev@gmail.com>
2024-07-09 11:42:11 -0300

    Config GraphQL to app.modules

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,9 @@
-import { Controller, Get } from '@nestjs/common'
-import { AppService } from './app.service'
+import { Query, Resolver } from '@nestjs/graphql'
 
-@Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello()
+@Resolver()
+export class AppResolver {
+  @Query(() => String)
+  hello(): string {
+    return 'hello dev!'
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,23 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ConfigModule } from '@nestjs/config'
 import { DatabaseModule } from './database/database.module'
 import { GraphQLModule } from '@nestjs/graphql'
-import path from 'path'
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+import path from 'node:path'
+import { AppResolver } from './app.controller'
+import { AuthorsModule } from './authors/authors.module'
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     DatabaseModule,
-    GraphQLModule.forRoot({
-      typePaths: ['./**/*.graphql'],
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
       autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
     }),
+    AuthorsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppResolver],
 })
 export class AppModule {}
